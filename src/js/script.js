a5616050c572b3cb580bdef9319c433a59e2b6a5
Mykola Oliynyk <oliinik.mikola@gmail.com>
2023-02-05 21:02:55 +0200

    Footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,26 @@
 $(document).ready(function(){
 		$('.carousel__inner').slick({
 				speed: 1200,
-				adaptiveHeight: true,
+				// adaptiveHeight: true,
+				dots: false,
 				prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></img> </button>',
 				nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></img> </button>',
 				responsive: [
 						{
 							breakpoint: 991,
 							settings: {
-								dots: false,
 								arrows: false
 							}
 						},
 						{
 							breakpoint: 767,
 							settings: {
-								dots: false,
 								arrows: false
 							}
 						},
 						{
 							breakpoint:  575,
 							settings: {
-								dots: false,
 								arrows: false
 							}
 						}
@@ -34,23 +32,6 @@ $(document).ready(function(){
 				.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 				.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 		});
-
-
-		// $('.catalog-item__link').each(function(i){
-		// 		$(this).on('click', function(e){
-		// 			e.preventDefault();
-		// 			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-		// 			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-		// 		})
-		// });
-
-		// $('.catalog-item__back').each(function(i){
-		// 	$(this).on('click', function(e){
-		// 		e.preventDefault();
-		// 		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-		// 		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-		// 	})
-		// });
 
 		function toggleSlide(item) {
 			$(item).each(function(i){
